55d681d140b6132d1afaa974d18d77c4d55fa24b
ligi <ligi@ligi.de>
2019-11-27 16:01:19 +0900

    Improve code by using EIP1450 with generated code from Kethereum

--- src/main/kotlin/net/goerli/pusher/Main.kt
+++ src/main/kotlin/net/goerli/pusher/Main.kt
@@ -1,17 +1,20 @@
 package net.goerli.pusher
 
+import org.kethereum.DEFAULT_GAS_PRICE
 import org.kethereum.crypto.createEthereumKeyPair
 import org.kethereum.eip155.signViaEIP155
 import org.kethereum.erc681.ERC681
 import org.kethereum.erc681.generateURL
 import org.kethereum.functions.encodeRLP
 import org.kethereum.keystore.api.InitializingFileKeyStore
-import org.kethereum.model.*
-import org.kethereum.rpc.EthereumRPC
+import org.kethereum.model.Address
+import org.kethereum.model.ChainId
+import org.kethereum.model.SignedTransaction
+import org.kethereum.rpc.HttpEthereumRPC
 import org.kethereum.rpc.model.BlockInformation
+import org.komputing.kethereum.erc1450.ERC1450TransactionGenerator
 import org.walleth.console.barcodes.printQR
 import org.walleth.khex.clean0xPrefix
-import org.walleth.khex.hexToByteArray
 import org.walleth.khex.toHexString
 import java.io.File
 import java.lang.Thread.sleep
@@ -23,19 +26,21 @@ private val FAUCET_ADDRESS = listOf(
     "0x8c1e1e5b47980d214965f3bd8ea34c413e120ae4" // social
 )
 
-fun main() {
+val TOKEN_CONTRACT_ADDRESS = Address("0x7af963cF6D228E564e2A0aA0DdBF06210B38615D")
+val rpc = HttpEthereumRPC("https://in3.slock.it/goerli/nd-3")
+val chain = ChainId(5)
 
-    val rpc = EthereumRPC("https://in3.slock.it/goerli/nd-3")
-
-    val keyStore = InitializingFileKeyStore(File("keystore")).apply {
-        if (getAddresses().isEmpty()) {
-            println("creating new keypair")
-            addKey(createEthereumKeyPair())
-        }
+val keyStore = InitializingFileKeyStore(File("keystore")).apply {
+    if (getAddresses().isEmpty()) {
+        println("creating new keypair")
+        addKey(createEthereumKeyPair())
     }
+}
 
-    val address = keyStore.getAddresses().first()
-    val keyPair = keyStore.getKeyForAddress(address)!!
+val address = keyStore.getAddresses().first()
+val keyPair = keyStore.getKeyForAddress(address)!!
+
+fun main() {
 
     val erc681 = ERC681(address = address.hex)
     printQR(erc681.generateURL())
@@ -61,32 +66,7 @@ fun main() {
                 println("tx from faucet: " + it.transaction.to)
 
                 it.transaction.to?.let { toAddress ->
-
-                    val txCount = rpc.getTransactionCount(address.hex, "latest")
-                    val chain = ChainDefinition(ChainId(5), "GOR")
-                    val transaction = Transaction().copy(
-                        chain = chain,
-                        from = address,
-                        to = Address("0x7af963cF6D228E564e2A0aA0DdBF06210B38615D"),
-                        value = ZERO,
-                        gasLimit = BigInteger("100000"),
-                        nonce = BigInteger(txCount?.result?.clean0xPrefix(), 16),
-                        input = ("40c10f19"
-                                + "0".repeat(24) + toAddress.cleanHex
-                                + "000000000000000000000000000000000000000000000016c4abbebea0100000"
-                                ).hexToByteArray().toList()
-                    )
-                    val signature = transaction.signViaEIP155(keyPair, chain)
-
-                    val tx = transaction.encodeRLP(signature).toHexString()
-
-                    val sendTx = rpc.sendRawTransaction(tx)
-
-                    if (sendTx?.result != null) {
-                        println("send tx " + sendTx.result)
-                    } else {
-                        println("send tx error " + sendTx?.error)
-                    }
+                    sendTransaction(toAddress)
                 }
             }
 
@@ -94,5 +74,30 @@ fun main() {
         }
 
         sleep(2000)
+    }
+}
+
+private fun sendTransaction(toAddress: Address) {
+    val txCount = rpc.getTransactionCount(address.hex, "latest")
+
+    val mintableToken = ERC1450TransactionGenerator(TOKEN_CONTRACT_ADDRESS)
+
+    val transaction = mintableToken.mint(toAddress, BigInteger("420000000000000000000")).copy(
+        chain = chain.value,
+        value = ZERO,
+        nonce = BigInteger(txCount?.result?.clean0xPrefix(), 16),
+        gasPrice = DEFAULT_GAS_PRICE,
+        gasLimit = BigInteger("100000")
+    )
+    val signature = transaction.signViaEIP155(keyPair, chain)
+
+    val tx = transaction.encodeRLP(signature).toHexString()
+
+    val sendTx = rpc.sendRawTransaction(tx)
+
+    if (sendTx?.result != null) {
+        println("send tx " + sendTx.result)
+    } else {
+        println("send tx error " + sendTx?.error)
     }
 }
